4caa03b098448fb046f5a65f252db26ac080e75b
Lincoln de Sousa <lincoln@comum.org>
2015-02-03 20:42:32 -0200

    add new regions

--- pnad2013.py
+++ pnad2013.py
@@ -15,6 +15,37 @@ CONVERT_TABLE = {c: lambda value: int(value or 0) for c in [
 ]}
 
 REGIOES_TABLE = {
+    # 11	Rondônia
+    # 12	Acre
+    # 13	Amazonas
+    # 14	Roraima
+    # 15	Pará
+    # 16	Amapá
+    # 17	Tocantins
+    # 21	Maranhão
+    # 22	Piauí
+    # 23	Ceará
+    # 24	Rio Grande do Norte
+    # 25	Paraíba
+    # 26	Pernambuco
+    # 27	Alagoas
+    # 28	Sergipe
+    # 29	Bahia
+    # 31	Minas Gerais
+    # 32	Espírito Santo
+    # 33	Rio de Janeiro
+    # 35	São Paulo
+    # 41	Paraná
+    # 42	Santa Catarina
+    # 43	Rio Grande do Sul
+    # 50	Mato Grosso do Sul
+    # 51	Mato Grosso
+    # 52	Goiás
+    # 53	Distrito Federal
+    'norte': [11, 12, 13, 14, 15, 16, 17],
+    'norteste': [21, 22, 23, 24, 25, 26, 27, 28, 29],
+    'sul': [41,42, 43],
+    'centro-oeste': [50, 51, 52, 53],
     'norte': [11, 12, 13, 14, 15, 16, 17, 21, 22],
 }
 

c2c007a5770ee0098fdc611d39c45a8bc075f47c
Lincoln de Sousa <lincoln@comum.org>
2016-08-02 00:59:01 -0400

    totally ignore the offset based on line count

--- convert.py
+++ convert.py
@@ -1,61 +1,68 @@
 #!/usr/bin/env python
-# -*- Coding: utf-8; -*-
-
+# -*- coding: utf-8; -*-
 import io
 import os
 import sys
 
-def get_vars(filepath):
+
+def get_var(line):
+    # Read
+    position, rest = line.split(' ', 1)
+    variable, rest = rest.strip().split(' ', 1)
+    size, rest = rest.strip().split(' ', 1)
+    comment = rest.replace('/*', '').replace('*/', '').strip()
+
+    # Convert
+    position = int(position.replace('@', ''))
+    variable = variable.strip()
+    size = int(float(size.replace('$', '')))
+
+    return {
+        'name': variable,
+        'position': position,
+        'size': size,
+        'comment': comment,
+    }
+
+
+def get_vars(varsfile):
     variables = []
-    total_size = 0
-    varsfile = io.open(filepath)
-
     for line in varsfile:
-        # Read
-        position, rest = line.split(' ', 1)
-        variable, rest = rest.strip().split(' ', 1)
-        size, rest = rest.strip().split(' ', 1)
-        comment = rest.replace('/*', '').replace('*/', '').strip()
+        variable = get_var(line)
+        variables.append(variable)
+    return variables
 
-        # Convert
-        position = int(position.replace('@', ''))
-        variable = variable.strip()
-        size = int(float(size.replace('$', '')))
 
-        variables.append({'name': variable, 'position': position, 'size': size})
-        total_size += size
-
-    varsfile.close()
-    return total_size, variables
-
-def read_var(fp, var, offset):
-    fp.seek(offset + var['position'] - 1) # 1 index based
+def read_var(fp, var):
+    fp.seek(var['position'] - 1) # 1 index based
     return fp.read(var['size'])
 
-def read_row(fp, variables, offset):
+
+def read_row(fp, variables):
     columns = []
     for var in variables:
-        value = read_var(fp, var, offset)
+        value = read_var(fp, var)
         columns.append(value.strip())
     return columns
+
 
 def print_header(variables, separator):
     print(separator.join(x['name'].encode('utf-8') for x in variables))
 
-def main(vars_file, data_file):
-    data_fp = io.open(data_file)
-    row_size, variables = get_vars(vars_file)
-    total_row_number = os.stat(data_file).st_size / row_size
-    row_count = 0
 
+def main(vars_file, data_file):
+    vars_fp = io.open(vars_file)
+    data_fp = io.open(data_file)
+    variables = get_vars(vars_fp)
     separator = ','
 
     print_header(variables, separator)
-    while row_count <= total_row_number:
-        print(separator.join(read_row(data_fp, variables, row_size * row_count)))
-        row_count += 1
+    while True:
+        line = read_row(data_fp, variables)
+        print(separator.join(line))
 
     data_fp.close()
+    vars_fp.close()
 
 if __name__ == '__main__':
     main(*sys.argv[1:3])
